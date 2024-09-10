8393a55838cdc29cef7514fee3fa4ed93e6ceb42
Decio Ferreira <decio.jferreira@gmail.com>
2024-09-10 22:08:18 +0100

    revert changes to hello example

--- examples/src/Hello.elm
+++ examples/src/Hello.elm
@@ -3,10 +3,5 @@ module Hello exposing (main)
 import Html exposing (text)
 
 
-fn1 : { bar : String, foo : String } -> String
-fn1 { bar, foo } =
-    bar ++ foo 
-
-
 main =
-    text (fn1 { bar = "lo!", foo = "Hel" })
+    text "Hello!"
