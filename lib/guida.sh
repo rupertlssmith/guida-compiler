14e59cd21df5f987c5b0429e22fa6950a2d79849
Decio Ferreira <decio.jferreira@gmail.com>
2024-12-11 22:58:44 +0000

    fixes #9 self-hosted environment

--- lib/guida.sh
+++ lib/guida.sh
@@ -7,7 +7,7 @@ set -e
 js="bin/guida.js"
 min="bin/guida.min.js"
 
-elm make --optimize --output=$js $@
+guida make --optimize --output=$js $@
 
 uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
 
