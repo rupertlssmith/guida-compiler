a5807eb469705fed5802a803c6af3add92bce45e
Decio Ferreira <decio.jferreira@gmail.com>
2025-04-26 20:13:17 +0100

    Refactor REPL tests to use a common run function for better readability and maintainability

--- tests/repl.test.js
+++ tests/repl.test.js
@@ -3,19 +3,26 @@ const path = require("node:path");
 
 describe("repl", () => {
     test("1 + 1", (done) => {
-        const repl = child_process.spawn("./bin/index.js", ["repl"], {
-            cwd: path.join(__dirname, ".."),
-            stdio: "pipe"
-        });
+        run("1 + 1", "\x1B[95m2\x1B[0m\x1B[90m : number\x1B[0m\n", done);
+    }, 120_000);
 
-        repl.stdout.on("data", (data) => {
-            if (data.toString() === "> ") {
-                repl.stdin.write("1 + 1\n");
-                repl.stdin.end();
-            } else if (data.toString() === "\x1B[95m2\x1B[0m\x1B[90m : number\x1B[0m\n") {
-                repl.kill();
-                done();
-            }
-        });
-    }, 200000);
+    test("string", (done) => {
+        run("\"Hello, World!\"", "\x1B[93m\"Hello, World!\"\x1B[0m\x1B[90m : String\x1B[0m\n", done);
+    }, 120_000);
 });
+
+const run = (input, output, done) => {
+    const repl = child_process.spawn("./bin/index.js", ["repl"], {
+        cwd: path.join(__dirname, ".."),
+        stdio: "pipe"
+    });
+
+    repl.stdout.on("data", (data) => {
+        if (data.toString() === "> ") {
+            repl.stdin.write(input + "\n");
+        } else if (data.toString() === output) {
+            repl.kill();
+            done();
+        }
+    });
+}
